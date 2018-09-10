09a8c5dbcd16dbeab9defbbacef6568e204816b8
Sprachforum Heinrich Heine Düsseldorf <43094489+sprachforum@users.noreply.github.com>
2018-09-10 06:22:00 +0200

    restoring

--- assets/hardy.js
+++ assets/hardy.js
@@ -14,5 +14,22 @@
 Art = 'Kurse' ;
 Thema = 'TestDaF' ;
 
-alert('<h1>Im Angebot: ' + Thema + '-' + Art + ' für ' + Daten[ Art ][ Thema ][ 'Preis' ] + ' € !</h1>');
+document.write('<h1>Im Angebot: ' + Thema + '-' + Art + ' für ' + Daten[ Art ][ Thema ][ 'Preis' ] + ' € !</h1>');
 
+document.write( ''
+  + '<h2>' + Daten[ Art ][ Thema ][ 'Termine' ].length + ' Termine:</h2>'
+  + '<ul>'
+);
+for ( i=0; i < Daten[ Art ][ Thema ][ 'Termine' ].length; i++ ) {
+document.write( ''
+  + '<li>'
+  + Daten[ Art ][ Thema ][ 'Termine' ][ i ]
+  + ' <a href="">Jetzt anmelden</a>'
+  + '</li>'
+);
+}
+document.write( ''
+  + '</ul>'
+);
+
+document.write('<p>(alle ' + Art + ': ' + Object.keys( Daten[ Art ] ) + ' )</p>');
